3812ebff6afcecebbe5bebaa1b422226b8b6ba16
Tian Group Inv. <ishimwechris94@gmail.com>
2026-09-09 00:46:40 +0200

    refactor: use fixed manual translations only

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,3 +1,2 @@
 'use client';
-import AutoTranslate from '@/components/AutoTranslate';
-export default function Providers({children}:{children:React.ReactNode}){return <><AutoTranslate/>{children}</>}
+export default function Providers({children}:{children:React.ReactNode}){return <>{children}</>}
